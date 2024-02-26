35689b0a4bf059202821501fc8d76f640c363e35
Shubhamrawat23 <shubhamrawat2311@gmail.com>
2024-02-27 02:49:03 +0530

    add responsive designs of phone and tablet

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,17 @@
-import React  from "react";
+import React, { useState }  from "react";
 import '../Header/Header.scss'
 import SearchImg from '../../Images/SearchImg.svg'
 
 export default function Header(){
+    const [searchValue,setSearchValue] = useState("")
+    const [inputField,setInputField] = useState(false)
+    console.log(searchValue);
     return (
         <header>
             <ul>
-                <li><img src={SearchImg} alt="searchImg" id="searchImg"/><input type="text" name="search" id="search"/></li>
+                <li><img src={SearchImg} alt="searchImg" id="searchImg" style={{display:searchValue !== ""?"none":"block"}} onClick={()=>{setInputField(!inputField)}}/>
+                <input type="text" name="search" id={inputField?"R_search":"search"} value={searchValue} onChange={(e)=>setSearchValue(e.target.value)} />
+                </li>
                 <li id="categories">Categories</li>
                 <li id="webBuilder">Website Builders</li>
                 <li id="deals">Today's deals</li>
--- src/Components/Main/DifferentTools/AllTools.js
+++ src/Components/Main/DifferentTools/AllTools.js
@@ -4,13 +4,13 @@ import "./AllTools.scss"
 
 export default function AllTools(){
     return (
-        <div>
+        <>
             <BoxLayout toolName="Tools" id="tool1"/>
             <BoxLayout toolName="AWS Builder" id="tool2"/>
             <BoxLayout toolName="Start Build" id="tool3"/>
             <BoxLayout toolName="Build Supplies" id="tool4"/>
             <BoxLayout toolName="Tooling" id="tool5"/>
             <BoxLayout toolName="BlueHosting" id="tool6"/>
-        </div>
+        </>
     )
 }
